9b3c4d0e26c6bd96d667c8a1ea2b6e37b71c1c0c
Sarvesh MS <ms.sarveshsarvesh.2006@gmail.com>
2026-04-06 12:56:23 +0530

    Export project status updater

--- client/src/api/projects.js
+++ client/src/api/projects.js
@@ -4,3 +4,7 @@ export const listProjects = () => api.get("/api/projects").then((res) => res.dat
 export const createProject = (data) => api.post("/api/projects", data).then((res) => res.data);
 export const updateProject = (id, data) => api.put(`/api/projects/${id}`, data).then((res) => res.data);
 export const deleteProject = (id) => api.delete(`/api/projects/${id}`).then((res) => res.data);
+export const updateProjectStatus = (id, status) =>
+  api.patch(`/api/projects/${id}/status`, { status }).then((res) => res.data);
+export const updateProjectStatus = (id, status) =>
+  api.patch(`/api/projects/${id}/status`, { status }).then((res) => res.data);
